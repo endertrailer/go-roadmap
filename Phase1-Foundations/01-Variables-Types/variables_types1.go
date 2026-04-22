86b40ac917ef608aea83e98467f9700ee5bfc67a
Test User <test@example.com>
2026-04-23 00:31:19 +0530

    progress day 1 - variables

---
+++ Phase1-Foundations/01-Variables-Types/variables_types1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// how to declare a variable type is automatically inferred
+	num := 10
+	fmt.Printf("%d \n", num)
+	// common types of variables
+	number := 12345 // int
+
+	text := "Phoenix A* is the biggest blackhole and has the mass roughly 100 billion times more than our sun" // string
+	decimals := 3.14159265                                                                                     // float64
+	print(number, "\n")
+	print(text, "\n")
+	print(decimals, "\n")
+}
