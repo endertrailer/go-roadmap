0d94f29aeaa4b06947bb86828335c84b2603101c
Test User <test@example.com>
2026-05-11 00:30:45 +0530

    create linkedList as a struct activity

--- Phase1-Foundations/02-Structs-Methods/structs_1.go
+++ Phase1-Foundations/02-Structs-Methods/structs_1.go
@@ -1,7 +1,53 @@
 package main
 
-type book struct {
-	Author string
-	Title  string
-	Pages  int
+import "fmt"
+
+type linkedList struct {
+	HeadNode *Node
+	TailNode *Node
+	// NextNode *Node
+}
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+func (n *Node) SetValue(newValue int) {
+	n.Value = newValue
+}
+
+func (l *linkedList) InsertNode(value int) {
+	if l.HeadNode == nil {
+		l.HeadNode = &Node{Value: value}
+		l.TailNode = &Node{Value: value}
+	} else {
+		node := l.HeadNode
+		for node.Next != nil {
+			node = node.Next
+		}
+		node.Next = &Node{Value: value}
+	}
+}
+
+func (l *linkedList) printList() {
+	head := l.HeadNode
+	for head != nil {
+		fmt.Printf("%d \n", head.Value)
+		head = head.Next
+	}
+}
+
+func runLinked() {
+	list := linkedList{}
+	list.InsertNode(10)
+	list.InsertNode(29)
+	list.InsertNode(34)
+	list.InsertNode(12)
+	list.InsertNode(54)
+	list.printList()
+}
+
+func main() {
+	runLinked()
 }
